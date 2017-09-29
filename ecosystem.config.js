135cf07b36222eaa3cccb3a96d959d62eee97330
Okura Masafumi <masafumi.o1988@gmail.com>
2017-09-29 12:25:10 +0900

    Remove dev deployment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -38,16 +38,5 @@ module.exports = {
       path : '/home/deploy/universal-boilerplate',
       'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
   }
 };
